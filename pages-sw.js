b254c2a6f64d7db28ec2f8294310bcbc2ca34a61
hu854573289-crypto <hu854573289@gmail.com>
2026-08-15 00:08:55 +0800

    Add GitHub Pages pages-sw.js

---
+++ pages-sw.js
@@ -0,0 +1,11 @@
+const CACHE = "wildsquad-pages-v1";
+const FILES = ["./", "./index.html", "./pages-game.css", "./pages-game.js", "./pages-manifest.webmanifest", "./public/assets/hero-world.webp", "./public/assets/hero-character.webp"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request).then(response => {
+    if (response.ok) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
+    return response;
+  }).catch(() => caches.match(event.request).then(hit => hit || caches.match("./index.html"))));
+});
